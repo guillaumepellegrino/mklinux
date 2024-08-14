a4df857bee3cb7297b50b7ff8c4e45f10666d087
Guillaume PELLEGRINO <guillaume.pellegrino@yahoo.com>
2024-08-14 15:37:04 +0200

    add udp binding on port 50 for qemu

--- config/x86_64/runqemu.sh
+++ config/x86_64/runqemu.sh
@@ -4,7 +4,7 @@ IMAGE="$1"
 
 set -x
 qemu-system-x86_64 \
-    -nic user,ipv6=off,mac=52:54:98:76:54:32,hostfwd=tcp::$CONFIG_SSH_PORT-10.0.0.10:22 \
+    -nic user,ipv6=off,mac=52:54:98:76:54:32,hostfwd=tcp::$CONFIG_SSH_PORT-10.0.0.10:22,hostfwd=udp::32050-10.0.0.10:50 \
     -no-reboot -nographic \
     -kernel component/linux/build/arch/x86_64/boot/bzImage \
     -drive file="$IMAGE",format=raw \
